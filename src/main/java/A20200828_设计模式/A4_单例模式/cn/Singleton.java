8791b3d1710b83e9d1f559c20bdf4fbe914a314d
a982338665 <982338665@qq.com>
2020-08-28 17:37:12 +0800

    单例

---
+++ src/main/java/A20200828_设计模式/A4_单例模式/cn/Singleton.java
@@ -0,0 +1,14 @@
+package A20200828_设计模式.A4_单例模式.cn;
+
+public class Singleton {
+    private static class SingletonHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    private Singleton() {
+    }
+
+    public static final Singleton getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+}
